44d4929142fd3d685295525d9c32bf4332d53356
sriramtadepalli <31714276+sriramtadepalli@users.noreply.github.com>
2017-10-09 13:45:56 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, abort
 import os
+import jinja2
 import cgi
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
  
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
+
+@app.route("/")
+def index():
+    template = jinja_env.get_template('hello_form.html')
+    return template.render()
+
+@app.route("/hello", methods=['POST'])
+def hello():
+    first_name = request.form['first_name']
+    template = jinja_env.get_template('hello_greeting.html')
+    return template.render(name=first_name)
 
  
 form = """
