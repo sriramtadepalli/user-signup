2e72d45b088dbbbd911d6acd6ae41af05e7248cb
sriramtadepalli <31714276+sriramtadepalli@users.noreply.github.com>
2017-10-09 15:34:36 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -11,17 +11,6 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-@app.route("/")
-def index():
-    template = jinja_env.get_template('hello_form.html')
-    return template.render()
-
-@app.route("/hello", methods=['POST'])
-def hello():
-    first_name = request.form['first_name']
-    template = jinja_env.get_template('hello_greeting.html')
-    return template.render(name=first_name)
-
  
 form = """
 <!doctype html>
@@ -55,7 +44,16 @@ form = """
 </html>
 """
  
+@app.route("/")
+def index():
+    template = jinja_env.get_template('hello_form.html')
+    return template.render()
 
+@app.route("/hello", methods=['POST'])
+def hello():
+    first_name = request.form['first_name']
+    template = jinja_env.get_template('hello_greeting.html')
+    return template.render(name=first_name)
  
 @app.route('/')
 def home():
